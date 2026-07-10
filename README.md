f950423bb485bb9fbc762a909d6134c22f6d114f
riseulbigurl-rgb <riseulbigurl@gmail.com>
2026-07-11 06:29:06 +0700

    Start repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# dchmmm
+
+[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-lrs5j7pk)
---
+++ public/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'dampingcare-v1';
+const PRECACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/cropped_circle_image_(1).webp',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
